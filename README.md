7408423723068f5e0e4af4fed2b39c69afc57ea4
Kiran <kiran.cshet@gmail.com>
2023-01-03 20:54:53 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Create-Zip-Python
+
+This Python script create  the image by 
+
+
+## Table of Contents
+
+- [Description](#description)
+- [Prerequisites](#prerequisites)
+- [Project Structure](#structure)
+- [Usage](#usage)
+- [Contact](#contact)
+
+## Description <a name = "description"></a>
+
+This Python script takes an input folder and creates a zip file and stores in the output folder. As a use case, this python script does the following:
+
+- Reads all the files within the input folder.
+- The input folder comprises of both files and folders
+- Create a zip file within output folder
+- Unzip the zip file in the output folder 
+
+
+## Usage <a name = "usage"></a>
+
+python create_zip.py
+
+
+## Project Structure  <a name = "structure"></a>
+
+The entire project follows the below folder structure:
+
+    .
+    ├── create_zip.py               # Main Python script to create zip file
+    ├── input/                      # Input folder comprising files and folder
+    ├── output/                     # Output folder where zip file will be saved
+    └── README.md
+
+
+## Contact <a name = "contact"></a>
+
+Follow me for more updates here:
+
+- [Article](https://www.sapnaedu.com)
+- [Twitter](https://twitter.com/sapnaedu)
+- [Website](https://www.sapnaedu.com)
+- [LinkedIn](https://www.linkedin.com/in/kiranchandrashekhar/)
+- [Email](mailto:kiran.chandrashekhar@gmail.com)
---
+++ create_zip.py
@@ -0,0 +1,83 @@
+#-------------------------------------------------------------------------------
+# Name:         create_zip.py
+# Purpose:      This Python script takes an input folder and creates a zip file and stores in the output folder. 
+# Author:       Kiran Chandrashekhar
+# Created:      18-Dec-2022
+#-------------------------------------------------------------------------------
+
+import os
+from random import randint
+import zipfile
+import shutil
+
+class CreateZip:
+    def __init__(self):
+        pass
+
+    def get_all_files(self, directory:str)->list:
+        """
+        Get all the files from the directory
+        """
+        complete_file_list = []
+        for dirname, subdirs, files in os.walk(directory):                
+            file_list = [os.path.join(dirname, file) for file in files]
+            complete_file_list.extend(file_list)
+      
+        return complete_file_list
+
+    #-------------------------------------------------#
+    #   Create Zip File from the folder - v1          #
+    #-------------------------------------------------#
+
+    def create_zip_file(self, directory:str)->str:
+        """
+        Create a zip file from the list of all the file from the 
+        specified directory
+        """
+        file_list = self.get_all_files(directory)
+        zip_file = f"{os.getcwd()}/output/{randint(100_000,999_999)}.zip"
+        
+        with zipfile.ZipFile(zip_file, "w") as zf:
+            for file in file_list:                
+                relative_path = os.path.relpath(file, directory)     
+                zf.write(file, relative_path)
+        return zip_file
+
+
+    #-------------------------------------------------#
+    #   Create Zip File from the folder - v2          #
+    #-------------------------------------------------#
+
+    def create_zip_file_v2(self, directory:str)->str:
+        """
+        Create a zip file from the list of all the file from the 
+        specified directory
+        """
+        zip_file = f"{os.getcwd()}/output/{randint(100_000,999_999)}"
+        zip_path = shutil.make_archive(zip_file, 'zip', directory)
+        return zip_path
+
+    #-------------------------------------------------#
+    #  Unzip the file                                 #
+    #-------------------------------------------------#
+    def unzip_file(self, zip_file:str)->str:
+        """
+        Unzip the file and save the extracted files in the output folders
+        """
+        output_folder = f"{os.getcwd()}/output/{randint(100_000,999_999)}"
+        with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+            zip_ref.extractall(output_folder)  
+        return output_folder
+
+def main():
+
+    obj = CreateZip()
+    directory = f"{os.getcwd()}/input/"
+    zip_file_path = obj.create_zip_file(directory)
+    output_folder = obj.unzip_file(zip_file_path)
+    print(output_folder)
+   
+    
+if __name__ == '__main__':
+    main()
+    print("Done")
